0e6c2fa516fdd60aab5ce6a6f64c5f57730cd92c
wojtek pasieka <wojtek.pasiu@gmail.com>
2024-07-12 18:59:59 +0200

    add functions: cleanInputString, isInvalidInput

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ let isError = false;
 
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
 
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetId = '#' + entryDropdown.value;
+  const targetInputContainer = document.querySelector(targetId + ' .input-container');
 }
